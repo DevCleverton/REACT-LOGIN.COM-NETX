c99f70f17fb5c2653dc783206d9ed282eade58d6
Cleverton da Rocha <56972170+DevCleverton@users.noreply.github.com>
2022-07-26 12:42:02 +0000

    ok

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Heading, Grid, Flex, Link, Button, Text } from '@chakra-ui/core'
+import Divider from '../components/Divider'
+import Input from '../components/Input'
 
 ReactDOM.render(<App />, document.getElementById("root"));
